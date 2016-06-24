7920c65fef57ab97a5db05d40397fe07308c1ad9
dugup46 <dugup46@gmail.com>
2016-06-24 17:10:56 -0400

    Updated all files from local machine

--- resources/botkit/slack_bot.js
+++ resources/botkit/slack_bot.js
@@ -4,36 +4,63 @@
           \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
            \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
             \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
+
+
 This is a sample Slack bot built with Botkit.
+
 This bot demonstrates many of the core features of Botkit:
+
 * Connect to Slack using the real time API
 * Receive messages based on "spoken" patterns
 * Reply to messages
 * Use the conversation system to ask questions
 * Use the built in storage system to store and retrieve information
   for a user.
+
 # RUN THE BOT:
+
   Get a Bot token from Slack:
+
     -> http://my.slack.com/services/new/bot
+
   Run your bot from the command line:
+
     token=<MY TOKEN> node slack_bot.js
+
 # USE THE BOT:
+
   Find your bot inside Slack to send it a direct message.
+
   Say: "Hello"
+
   The bot will reply "Hello!"
+
   Say: "who are you?"
-  The bot will tell you its name, where it is running, and for how long.
+
+  The bot will tell you its name, where it running, and for how long.
+
   Say: "Call me <nickname>"
+
   Tell the bot your nickname. Now you are friends.
+
   Say: "who am I?"
+
   The bot will tell you your nickname, if it knows one for you.
+
   Say: "shutdown"
+
   The bot will ask if you are sure, and then shut itself down.
+
   Make sure to invite your bot into other channels using /invite @<my bot>!
+
 # EXTEND THE BOT:
+
   Botkit has many features for building cool and useful bots!
+
   Read all about it here:
+
     -> http://howdy.ai/botkit
+
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 
@@ -42,38 +69,17 @@ if (!process.env.token) {
     process.exit(1);
 }
 
-var Botkit = require('./resources/botkit/lib/Botkit.js');
+var Botkit = require('./lib/Botkit.js');
 var os = require('os');
 
 var controller = Botkit.slackbot({
-    debug: true
+    debug: true,
 });
 
 var bot = controller.spawn({
     token: process.env.token
 }).startRTM();
 
-controller.hears(['help'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-    bot.api.reactions.add({
-        timestamp: message.ts,
-        channel: message.channel,
-        name: 'robot_face',
-    }, function(err, res) {
-        if (err) {
-            bot.botkit.log('Failed to add emoji reaction :(', err);
-        }
-    });
-
-
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Hello ' + user.name + '!!');
-        } else {
-            bot.reply(message, 'Hello.');
-        }
-    });
-});
 
 controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
 
@@ -177,6 +183,34 @@ controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention
                 }
             });
         }
+    });
+});
+
+
+controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
+
+    bot.startConversation(message, function(err, convo) {
+
+        convo.ask('Are you sure you want me to shutdown?', [
+            {
+                pattern: bot.utterances.yes,
+                callback: function(response, convo) {
+                    convo.say('Bye!');
+                    convo.next();
+                    setTimeout(function() {
+                        process.exit();
+                    }, 3000);
+                }
+            },
+        {
+            pattern: bot.utterances.no,
+            default: true,
+            callback: function(response, convo) {
+                convo.say('*Phew!*');
+                convo.next();
+            }
+        }
+        ]);
     });
 });
 
