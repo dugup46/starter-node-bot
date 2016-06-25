f278fe9428810bd1cc8eb2618e9dfe4fa7d3c2fa
dugup46 <dugup46@gmail.com>
2016-06-25 00:20:49 -0400

    Bot 1.0!

--- index.js
+++ index.js
@@ -1,39 +1,5 @@
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-           ______     ______     ______   __  __     __     ______
-          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
-          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
-           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
-            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
-This is a sample Slack bot built with Botkit.
-This bot demonstrates many of the core features of Botkit:
-* Connect to Slack using the real time API
-* Receive messages based on "spoken" patterns
-* Reply to messages
-* Use the conversation system to ask questions
-* Use the built in storage system to store and retrieve information
-  for a user.
-# RUN THE BOT:
-  Get a Bot token from Slack:
-    -> http://my.slack.com/services/new/bot
-  Run your bot from the command line:
-    token=<MY TOKEN> node slack_bot.js
-# USE THE BOT:
-  Find your bot inside Slack to send it a direct message.
-  Say: "Hello"
-  The bot will reply "Hello!"
-  Say: "who are you?"
-  The bot will tell you its name, where it is running, and for how long.
-  Say: "Call me <nickname>"
-  Tell the bot your nickname. Now you are friends.
-  Say: "who am I?"
-  The bot will tell you your nickname, if it knows one for you.
-  Say: "shutdown"
-  The bot will ask if you are sure, and then shut itself down.
-  Make sure to invite your bot into other channels using /invite @<my bot>!
-# EXTEND THE BOT:
-  Botkit has many features for building cool and useful bots!
-  Read all about it here:
-    -> http://howdy.ai/botkit
+WEATHER BOT by dugup46!
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 
@@ -46,7 +12,7 @@ var Botkit = require('./resources/botkit/lib/Botkit.js');
 var os = require('os');
 
 var controller = Botkit.slackbot({
-    debug: true
+    debug: false
 });
 
 var bot = controller.spawn({
@@ -58,155 +24,85 @@ controller.hears(['help'], 'direct_message,direct_mention,mention', function(bot
     bot.api.reactions.add({
         timestamp: message.ts,
         channel: message.channel,
-        name: 'robot_face',
+        name: 'sos',
     }, function(err, res) {
         if (err) {
             bot.botkit.log('Failed to add emoji reaction :(', err);
         }
     });
-
-
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Hello ' + user.name + '!!');
-        } else {
-            bot.reply(message, 'Hello.');
-        }
-    });
+    
+    var help = 'I will respond to the following messages: \n' +
+      '`@' + bot.identity.name + ' radar <location>` to provide a base reflectivity scan of a given location.\n' +
+      '`@' + bot.identity.name + ' bvscan <location>` to provide a base velocity scan of a given location.\n' +
+      '`@' + bot.identity.name + ' What\'s the weather in <location>` provide temperature and conditions.\n' +
+      '`@' + bot.identity.name + ' help` to see this again.'
+	bot.reply(message, help)
 });
 
-controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-    bot.api.reactions.add({
-        timestamp: message.ts,
-        channel: message.channel,
-        name: 'robot_face',
-    }, function(err, res) {
-        if (err) {
-            bot.botkit.log('Failed to add emoji reaction :(', err);
-        }
-    });
-
+controller.hears(['radar'], ['direct_message', 'direct_mention'], function (bot, message) {
 
     controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Hello ' + user.name + '!!');
-        } else {
-            bot.reply(message, 'Hello.');
-        }
+        
+	bot.startConversation(message, function(err, convo) {
+
+		convo.ask('Which radar site do you want?', function(response, convo) {
+
+			convo.next();
+
+		}, {'key': 'radarsite'}); // store the results in a field called nickname
+
+		convo.ask('Which type of radar scan do you want?', function(response, convo) {
+
+			convo.next();
+
+		}, {'key': 'scantype'}); // store the results in a field called nickname
+		
+		convo.on('end', function(convo) {
+			if (convo.status == 'completed') {
+
+				var scantype = convo.extractResponse('scantype');
+				var radarsite = convo.extractResponse('radarsite');
+				
+				switch(scantype) {
+				case "reflect":
+					var scantype = "N0R"
+					break;
+				case "velocity":
+					var scantype = "N0V"
+					break;
+				case "motion":
+					var scantype = "N0S"
+					break;
+				case "1hour":
+					var scantype = "N1P"
+					break;
+				case "composite":
+					var scantype = "NCR"
+					break;
+				case "total":
+					var scantype = "NTP"
+					break;
+				default:
+					scantype = "N0R"
+				} 
+				
+				var text = "Here is the " + scantype + " scan from your requested location: "
+				var attachment = [{
+						"title": "For a direct link to the NWS page for " + radarsite + ", click here.",
+						"title_link": "http://www.weather.gov/" + radarsite + "/",
+						"text": text,
+						"fallback": text,
+						"image_url": "http://radar.weather.gov/ridge/RadarImg/" + scantype + "/" + radarsite + "_" + scantype + "_0.gif",
+						"color": "#7CD197",
+				}]
+
+				bot.reply(message, {
+					attachments: attachment
+				}, function (err, resp) {
+					console.log(err, resp)
+					})
+				}
+			});
+		});
     });
-});
-
-controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
-    var name = message.match[1];
-    controller.storage.users.get(message.user, function(err, user) {
-        if (!user) {
-            user = {
-                id: message.user,
-            };
-        }
-        user.name = name;
-        controller.storage.users.save(user, function(err, id) {
-            bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-        });
-    });
-});
-
-controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Your name is ' + user.name);
-        } else {
-            bot.startConversation(message, function(err, convo) {
-                if (!err) {
-                    convo.say('I do not know your name yet!');
-                    convo.ask('What should I call you?', function(response, convo) {
-                        convo.ask('You want me to call you `' + response.text + '`?', [
-                            {
-                                pattern: 'yes',
-                                callback: function(response, convo) {
-                                    // since no further messages are queued after this,
-                                    // the conversation will end naturally with status == 'completed'
-                                    convo.next();
-                                }
-                            },
-                            {
-                                pattern: 'no',
-                                callback: function(response, convo) {
-                                    // stop the conversation. this will cause it to end with status == 'stopped'
-                                    convo.stop();
-                                }
-                            },
-                            {
-                                default: true,
-                                callback: function(response, convo) {
-                                    convo.repeat();
-                                    convo.next();
-                                }
-                            }
-                        ]);
-
-                        convo.next();
-
-                    }, {'key': 'nickname'}); // store the results in a field called nickname
-
-                    convo.on('end', function(convo) {
-                        if (convo.status == 'completed') {
-                            bot.reply(message, 'OK! I will update my dossier...');
-
-                            controller.storage.users.get(message.user, function(err, user) {
-                                if (!user) {
-                                    user = {
-                                        id: message.user,
-                                    };
-                                }
-                                user.name = convo.extractResponse('nickname');
-                                controller.storage.users.save(user, function(err, id) {
-                                    bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-                                });
-                            });
-
-
-
-                        } else {
-                            // this happens if the conversation ended prematurely for some reason
-                            bot.reply(message, 'OK, nevermind!');
-                        }
-                    });
-                }
-            });
-        }
-    });
-});
-
-
-controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
-    'direct_message,direct_mention,mention', function(bot, message) {
-
-        var hostname = os.hostname();
-        var uptime = formatUptime(process.uptime());
-
-        bot.reply(message,
-            ':robot_face: I am a bot named <@' + bot.identity.name +
-             '>. I have been running for ' + uptime + ' on ' + hostname + '.');
-
-    });
-
-function formatUptime(uptime) {
-    var unit = 'second';
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'minute';
-    }
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'hour';
-    }
-    if (uptime != 1) {
-        unit = unit + 's';
-    }
-
-    uptime = uptime + ' ' + unit;
-    return uptime;
-}
+})
