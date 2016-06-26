cdce6739dc5cbf0a9aab6b628d7d1fafdb8d15f5
dugup46 <dugup46@gmail.com>
2016-06-25 23:39:57 -0400

    Main file for bot edits.  Last update 6/25

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var controller = Botkit.slackbot({
 });
 
 var bot = controller.spawn({
-    token: process.env.token
+    token: process.env.SLACK_TOKEN
 }).startRTM();
 
 controller.hears(['help'], 'direct_message,direct_mention,mention', function(bot, message) {
